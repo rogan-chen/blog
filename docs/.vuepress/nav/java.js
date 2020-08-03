57a1eda9a9363e573166a97a36c8eeb9c94349f8
Rogan <3355351461@qq.com>
2020-08-03 18:03:39 +0800

    目录

---
+++ docs/.vuepress/nav/java.js
@@ -0,0 +1,47 @@
+module.exports = {
+    text: 'Java',
+    items: [
+        {
+            text: 'JavaSE', items: [
+                { text: '面向对象', link: '/' },
+                { text: '枚举和异常', link: '/' },
+                { text: '集合框架', link: '/' },
+                { text: 'IO流', link: '/' },
+                { text: '多线程', link: '/' },
+                { text: '网络编程和反射', link: '/' },
+                { text: 'Java8新特性', link: '/' },
+            ]
+        },
+        {
+            text: 'JavaWeb', items: [
+                { text: 'JDBC', link: '/' },
+                { text: 'Servlet', link: '/' },
+                { text: 'JSP', link: '/' },
+                { text: 'JSTL', link: '/' },
+            ]
+        },
+        {
+            text: 'J2EE', items: [
+                { text: 'Spring', link: '/' },
+                { text: 'SpringMVC', link: '/' },
+                { text: 'MyBatis', link: '/' },
+                { text: 'Quartz', link: '/' },
+            ]
+        },
+        {
+            text: '多线程高并发', items: []
+        },
+        {
+            text: 'JVM虚拟机', items: []
+        },
+        {
+            text: '微服务', items: []
+        },
+        {
+            text: '性能优化', items: []
+        },
+        {
+            text: '分布式', items: []
+        },
+    ]
+};
